e2a4862deafbdc432162d9b53be0b8f66924fee2
Dobrin Ciprian <cipi_d08@yahoo.com>
2023-02-11 20:09:02 +0200

    New Model + correction plural

--- Catalog_Digital/curs_manager/models.py
+++ Catalog_Digital/curs_manager/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Elev(models.Model):
+    class Meta:
+        verbose_name_plural = "Elevi"
+        
+    nume = models.TextField()
+    prenume = models.TextField()
+    an = models.IntegerField()
+    telefon = models.IntegerField()
