8d5306a4a52d7651757df7df1e5590b3a2da8105
Dobrin Ciprian <cipi_d08@yahoo.com>
2023-02-19 18:30:16 +0200

    Render Templates in views with HTML

--- Catalog_Digital/curs_manager/views.py
+++ Catalog_Digital/curs_manager/views.py
@@ -7,22 +7,29 @@ def salut(request):
     return HttpResponse("Salut")
 
 def elevi(request):
-    lista_studenti = ["Sudent1", "Student2"] 
+    # lista_studenti = ["Sudent1", "Student2"] 
     lista_studenti = Elev.objects.all()
-    #studenti_formatat = ", ".join(lista_studenti)
-    # studenti_formatat = "<br />".join(lista_studenti)
-    lista_formatata = [elev.nume for elev in lista_studenti]
-    #lista_formatata = "<br />".join(lista_formatata)
-    output_string =""
-    for elev in lista_studenti:
-        rand = f"{elev.nume}, {elev.prenume}<br/>"
-        output_string += rand
-    return render(request, "student.html")
+    # #studenti_formatat = ", ".join(lista_studenti)
+    # # studenti_formatat = "<br />".join(lista_studenti)
+    # lista_formatata = [elev.nume for elev in lista_studenti]
+    # #lista_formatata = "<br />".join(lista_formatata)
+    # output_string =""
+    # for elev in lista_studenti:
+    #     rand = f"{elev.nume}, {elev.prenume}<br/>"
+    #     output_string += rand
+    context = {
+        "elevi": lista_studenti
+    }
+    return render(request, "student.html", context)
     return HttpResponse(output_string)
 
 
 def contact(request):
-    return render(request, "contact.html")
+    lista_elevi = Elev.objects.all()
+    context = {
+        "elevi" : lista_elevi
+    }
+    return render(request, "contact.html", context)
 
 
     
