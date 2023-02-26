8689f92ce3ae892b896e79a549a26c0f9e3fce9f
Dobrin Ciprian <cipi_d08@yahoo.com>
2023-02-26 15:26:57 +0200

    Update Elev Model

--- Catalog_Digital/curs_manager/models.py
+++ Catalog_Digital/curs_manager/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.core.exceptions import ValidationError
+from django.core.validators import MinLengthValidator
 # Create your models here.
 
 class Elev(models.Model):
@@ -8,5 +9,7 @@ class Elev(models.Model):
         
     nume = models.TextField()
     prenume = models.TextField()
-    an = models.IntegerField()
-    telefon = models.IntegerField()
+    an = models.IntegerField(default=1, db_index=True)
+    telefon = models.IntegerField(null=True, blank=True)
+    cnp_student = models.CharField(unique=True, null=True, blank=True, max_length=13,
+                                   validators=[MinLengthValidator(13)])
