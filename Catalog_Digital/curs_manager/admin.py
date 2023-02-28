eb51380b4b8db2785f1dd9ce558bbc6748462e80
Dobrin Ciprian <cipi_d08@yahoo.com>
2023-02-28 07:37:40 +0200

    New Admin models

--- Catalog_Digital/curs_manager/admin.py
+++ Catalog_Digital/curs_manager/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Elev, Adresa
+from .models import Elev, AdresNoua
 
 class ElevAdmin(admin.ModelAdmin):
     list_display = ("nume", "prenume", "an")
     list_filter = ("an", )
     search_fields = ("nume", "prenume")
     
+class AdresaAdmin(admin.ModelAdmin):
+    list_display = ("strada", "oras", "elev")
+    list_filter = ("elev"), 
+ 
 
 admin.site.register(Elev, ElevAdmin)
-admin.site.register(Adresa)
+admin.site.register(AdresNoua, AdresaAdmin )
--- Catalog_Digital/curs_manager/models.py
+++ Catalog_Digital/curs_manager/models.py
@@ -14,12 +14,29 @@ class Elev(models.Model):
     cnp_student = models.CharField(unique=True, null=True, blank=True, max_length=13,
                                    validators=[MinLengthValidator(13)])
     
+    def __str__(self):
+        return f"{self.nume} {self.prenume} AN: {self.an}"
+    
     
 class Adresa(Elev):
     class Meta:
         verbose_name_plural = "Adrese"
+        
+    
     strada = models.CharField(max_length=33)
     oras = models.CharField(max_length=30)
     judet = models.CharField(max_length=15)
     cod_postal = models.IntegerField()
     
+class AdresNoua(models.Model):
+    class Meta:
+        verbose_name_plural = "Adrese"
+        
+    elev = models.ForeignKey(Elev, on_delete=models.CASCADE)
+    strada = models.CharField(max_length=33)
+    oras = models.CharField(max_length=30)
+    judet = models.CharField(max_length=15)
+    cod_postal = models.IntegerField()
+    
+    def __str__(self):
+        return f"{self.strada} {self.elev}"
